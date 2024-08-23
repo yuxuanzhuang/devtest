2fac82abed060fb86e4acb6af81fadf35f1f97ec
Yuxuan Zhuang <yuxuan.zhuang@dbb.su.se>
2024-08-23 09:14:41 +0100

    set dev mda

--- devtest/tests/test_devtest.py
+++ devtest/tests/test_devtest.py
@@ -17,3 +17,10 @@ def test_mdanalysis_logo_length(mdanalysis_logo_text):
     """Example test using a fixture defined in conftest.py"""
     logo_lines = mdanalysis_logo_text.split("\n")
     assert len(logo_lines) == 46, "Logo file does not have 46 lines!"
+
+
+def test_mdanalysis_version():
+    """Test that a develop version of MDAnalysis is installed"""
+    import MDAnalysis as mda
+    # test it end with dev0
+    assert mda.__version__.endswith("dev0"), "MDAnalysis version is not a development version"
